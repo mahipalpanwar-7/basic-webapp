9de9f740da2fbe447b4c21018fa634a7d905b198
mahipalpanwar-7 <mahipalpanwar6354@gmail.com>
2026-07-11 11:03:09 +0530

    accessing template

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
-	"github.com/gorilla/mux"
+	"github.com/gorilla/mux" // used for URI parameters insted of query parameters
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "webapp")
-}
+var templates *template.Template // need templates to be accessible from routes  for simplicity global object is created
+
 func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html")) // instantiate the template object (for parse the code from the folder)
 	r := mux.NewRouter()
-	r.HandleFunc("/", handler).Methods("GET")
+	r.HandleFunc("/", indexHandler).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "index.html", nil)
 }
