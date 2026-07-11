1d4601af269a3840e035d4ca6772d40e94a5774f
mahipalpanwar-7 <mahipalpanwar6354@gmail.com>
2026-07-11 10:18:52 +0530

    using gorillamux

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mahipalpanwar-7/basic-webapp
+
+go 1.26.4
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
-func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w,"webapp")
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "webapp")
 }
-func main(){
-http.HandleFunc("/",handler)
-http.ListenAndServe(":8080",nil)
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
